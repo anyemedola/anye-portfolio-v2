d9ac508fd05fa6c47db8ed5785beb7144866f53b
Any Medola <anynhamedola@gmail.com>
2023-08-25 10:57:13 +0200

    qualification update

--- src/components/Qualification/Qualification.jsx
+++ src/components/Qualification/Qualification.jsx
@@ -9,83 +9,91 @@ function Qualification() {
         Here's little bit of my history in the IT Industry
       </span>
 
-      <div className="qualification_sections">
-        <div className="qualification_content">
-
+      <div className="qualification_container container">
+        <div className="qualification_sections">
+          <div className="qualification_content">
             <div className="qualification_data">
-                <div>
-                    <h3 className="qualification_title">Senior Frontend Developer</h3>
-                    <span className="qualification_subtitle">Capgemini</span>
-                    <div className="qualification_calendar">
-                        <i className="util-calendar-alt"></i> 2023 - Present
-                    </div>
+              <div>
+                <h3 className="qualification_title">
+                  Senior Frontend Developer
+                </h3>
+                <span className="qualification_subtitle">Capgemini</span>
+                <div className="qualification_calendar">
+                  <i className="util-calendar-alt"></i> 2023 - Present
                 </div>
+              </div>
 
-                <div>
-                    <span className="qualification_rounder"></span>
-                    <span className="qualification_line"></span>
-                </div>
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
             </div>
 
             <div className="qualification_data">
-                <div>
-                    <h3 className="qualification_title">Frontend Developer</h3>
-                    <span className="qualification_subtitle">Ambisig</span>
-                    <div className="qualification_calendar">
-                        <i className="util-calendar-alt"></i> 2022 - 2023
-                    </div>
-                </div>
+              <div></div>
 
-                <div>
-                    <span className="qualification_rounder"></span>
-                    <span className="qualification_line"></span>
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification_title">Frontend Developer</h3>
+                <span className="qualification_subtitle">Ambisig</span>
+                <div className="qualification_calendar">
+                  <i className="util-calendar-alt"></i> 2022 - 2023
                 </div>
+              </div>
             </div>
 
             <div className="qualification_data">
-                <div>
-                    <h3 className="qualification_title">Frontend Developer</h3>
-                    <span className="qualification_subtitle">S2 Pets</span>
-                    <div className="qualification_calendar">
-                        <i className="util-calendar-alt"></i> 2022 - 2022
-                    </div>
+              <div>
+                <h3 className="qualification_title">Frontend Developer</h3>
+                <span className="qualification_subtitle">S2 Pets</span>
+                <div className="qualification_calendar">
+                  <i className="util-calendar-alt"></i> 2022 - 2022
                 </div>
+              </div>
 
-                <div>
-                    <span className="qualification_rounder"></span>
-                    <span className="qualification_line"></span>
-                </div>
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
             </div>
 
             <div className="qualification_data">
-                <div>
-                    <h3 className="qualification_title">PHP Web Developer</h3>
-                    <span className="qualification_subtitle">Agência Zum</span>
-                    <div className="qualification_calendar">
-                        <i className="util-calendar-alt"></i> 2021 - 2022
-                    </div>
-                </div>
+              <div></div>
 
-                <div>
-                    <span className="qualification_rounder"></span>
-                    <span className="qualification_line"></span>
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
+              <div>
+                <h3 className="qualification_title">PHP Web Developer</h3>
+                <span className="qualification_subtitle">Agência Zum</span>
+                <div className="qualification_calendar">
+                  <i className="util-calendar-alt"></i> 2021 - 2022
                 </div>
+              </div>
             </div>
 
             <div className="qualification_data">
-                <div>
-                    <h3 className="qualification_title">PHP Web Developer</h3>
-                    <span className="qualification_subtitle">Atitude Comunicação</span>
-                    <div className="qualification_calendar">
-                        <i className="util-calendar-alt"></i> 2020 - 2021
-                    </div>
+              <div>
+                <h3 className="qualification_title">PHP Web Developer</h3>
+                <span className="qualification_subtitle">
+                  Atitude Comunicação
+                </span>
+                <div className="qualification_calendar">
+                  <i className="util-calendar-alt"></i> 2020 - 2021
                 </div>
+              </div>
 
-                <div>
-                    <span className="qualification_rounder"></span>
-                    <span className="qualification_line"></span>
-                </div>
+              <div>
+                <span className="qualification_rounder"></span>
+                <span className="qualification_line"></span>
+              </div>
             </div>
+          </div>
         </div>
       </div>
     </section>
